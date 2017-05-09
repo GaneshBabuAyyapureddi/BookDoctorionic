926429d52e7d4c609c081045898633ca9cd8c121
OFS1\balaji.ramesh <balaji.ramesh@object-frontier.com>
2017-05-09 15:52:52 +0530

    Added Doctor profile UI and functionality

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('bookDoctor', ['ionic', 'ui.router','ui.calendar','ui.bootstrap'])
+angular.module('bookDoctor', ['ionic', 'ui.router','ui.calendar','ui.bootstrap', 'ionic-ratings'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -44,9 +44,15 @@ angular.module('bookDoctor', ['ionic', 'ui.router','ui.calendar','ui.bootstrap']
                 controller: 'signupController'
             })
            .state('generalProfile', {
-                url:'generalProfile',               
+                url:'/generalProfile',               
                 templateUrl: 'Modules/Templates/GeneralPatientProfile.html',
                 controller:'generalPatientProfileController'
             }) 
+           .state('doctorProfile', {
+                url:'/doctorProfile',               
+                templateUrl: 'Modules/Templates/Profile.html',
+                controller:'ProfileController'
+            })
         $urlRouterProvider.otherwise('/loginPage');
 })
+
