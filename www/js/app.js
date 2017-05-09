3684ea7c94ff978d45f2f51e448018648a95938c
OFS1\rajeswari.babu <rajeswari.babu@object-frontier.com>
2017-05-09 18:47:55 +0530

    Doctor List Screen Ratings added

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('bookDoctor', ['ionic', 'ui.router','ui.calendar','ui.bootstrap'])
+angular.module('bookDoctor', ['ionic', 'ui.router','ui.calendar','ui.bootstrap', 'ionic-ratings'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -47,6 +47,11 @@ angular.module('bookDoctor', ['ionic', 'ui.router','ui.calendar','ui.bootstrap']
                 url:'generalProfile',               
                 templateUrl: 'Modules/Templates/GeneralPatientProfile.html',
                 controller:'generalPatientProfileController'
-            }) 
+            })
+            .state('doctorSearch', {
+                url:'/doctorSearch',               
+                templateUrl: 'Modules/Templates/DoctorSearch.html',
+                controller:'doctorSearchController'
+            })  
         $urlRouterProvider.otherwise('/loginPage');
 })
