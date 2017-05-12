f905e24b21fd349f9c401e29b43eed4a23fd104f
Daniel Jawahar <danielj@itp.objectfrontier.com>
2017-05-12 15:50:30 +0530

    changepswd screen

--- www/Modules/Controllers/MenuController.js
+++ www/Modules/Controllers/MenuController.js
@@ -1,6 +1,6 @@
 angular.module('bookDoctor')
 
-.controller("MenuController",function ($scope, $state) {
+.controller("MenuController",function ($scope, $state,$ionicModal) {
   $scope.goToHomeScreen = function() {
     $state.go('dashboard.homeScreen');
   }
@@ -16,9 +16,27 @@ angular.module('bookDoctor')
   $scope.goToNotification = function() {
     $state.go('notification');
   }
+  $scope.goToSettings = function() {
+    $state.go('settings');
+  }
     $scope.goToLogin = function() {
     $state.go('loginPage');
   }
 
+
+$ionicModal.fromTemplateUrl('password-modal.html', {
+        scope: $scope,
+        animation: 'slide-in-up'
+      }).then(function(modal) {
+        $scope.passwordModal = modal;
+      });
+
+      $scope.openPasswordModal = function() {
+        $scope.passwordModal.show();
+      };
+
+      $scope.closeModal = function() {
+         $scope.passwordModal.hide();
+      };
 })
 
