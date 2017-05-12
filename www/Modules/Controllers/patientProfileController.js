350dd6009388a7b1da0e6cc61408d08621cf83e6
Inferno43 <saravanakumar43.is@gmail.com>
2017-05-12 15:24:28 +0530

    Local changes

--- www/Modules/Controllers/patientProfileController.js
+++ www/Modules/Controllers/patientProfileController.js
@@ -1,7 +1,4 @@
 angular.module('bookDoctor')
-
 .controller("PatientProfileController", function($scope, $state, $ionicModal, $stateParams,$ionicHistory){
-     $scope.goBack = function() {
-	 $state.go('dashboard.homeScreen');
-    }
+    
 })
