1411d5174ab81f24b16958f03c158f0a730011b8
Inferno43 <saravanakumar43.is@gmail.com>
2017-05-12 14:53:53 +0530

    minor bug fixes

--- www/Modules/Controllers/patientProfileController.js
+++ www/Modules/Controllers/patientProfileController.js
@@ -1,4 +1,7 @@
 angular.module('bookDoctor')
-.controller("patientProfileController", function($scope, $state, $ionicModal, $stateParams,$ionicHistory){
-    
+
+.controller("PatientProfileController", function($scope, $state, $ionicModal, $stateParams,$ionicHistory){
+     $scope.goBack = function() {
+	 $state.go('dashboard.homeScreen');
+    }
 })
