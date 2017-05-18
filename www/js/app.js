973450294880c53ad42a6529b6097a2b709a8251
aswathhasina <aswath.hasina@object-frontier.com>
2017-05-18 12:07:18 +0530

    Removed unwanted code from menu template

--- www/js/app.js
+++ www/js/app.js
@@ -4,7 +4,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 
-angular.module('bookDoctor', ['ionic', 'ui.router','chart.js','ui.calendar','ui.bootstrap', 'ionic-ratings','ngCordova'])
+angular.module('bookDoctor', ['ionic', 'ui.router','chart.js','ui.calendar', 'ionic-ratings','ngCordova'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
