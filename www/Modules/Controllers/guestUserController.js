5ff26c8f2958b95b20e2a56ca2c5aa31b7509d6a
vin_ofs <vinothkumar.indirajit@object-frontier.com>
2017-05-31 17:54:25 +0530

    Implemented images swiping functionality in guest user page : By Ganesh Babu

--- www/Modules/Controllers/guestUserController.js
+++ www/Modules/Controllers/guestUserController.js
@@ -1,10 +1,30 @@
 angular.module('bookDoctor')
 
-.controller("guestUserController",function ($scope, $state, $ionicHistory) {
+.controller("guestUserController",function ($scope, $state, $ionicHistory, $ionicSlideBoxDelegate) {
 
 
   $scope.goBack = function() {
     $ionicHistory.goBack();
   }
+
+  $scope.images = $scope.images = ["img/LoginScreen.jpg","img/SignupScreen.jpg","img/ForgotPassword.jpg",
+                    "img/HomeScreenAppointments.jpg",
+                     "img/HomeScreenCalendar.jpg",
+                     "img/DoctorSearchScreen.jpg",
+                     "img/DoctorSearchFilter.jpg",
+                     "img/DoctorProfile.jpg",
+                     "img/DoctorRatings.jpg",
+                     "img/PatientProfileTabs.jpg",
+                     "img/PatientLabReports.jpg",
+                     "img/SettingsScreen.jpg",
+                     "img/ChangePassword.jpg",
+                     "img/ChangeThemeScreen.jpg",
+     
+    					];
+            setTimeout(function() {
+                $ionicSlideBoxDelegate.slide(0);
+                $ionicSlideBoxDelegate.update();
+                $scope.$apply();
+            });
 
 });
