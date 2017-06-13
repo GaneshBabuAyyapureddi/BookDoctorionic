a59e2ca53c6480bce65e7f89888f143968414bda
vin_ofs <vinothkumar.indirajit@object-frontier.com>
2017-06-13 19:05:41 +0530

    Slider dot colour dynamically  changes done as per selected theme . By Ganesh Babu

--- www/Modules/Controllers/guestUserController.js
+++ www/Modules/Controllers/guestUserController.js
@@ -1,7 +1,21 @@
 angular.module('bookDoctor')
 
-.controller("guestUserController",function ($scope, $state, $stateParams, $ionicHistory, $ionicSlideBoxDelegate) {
-
+.controller("guestUserController",function ($scope, $state, $stateParams, $ionicHistory, $ionicSlideBoxDelegate,$rootScope) {
+    //  $scope.mystyles1 = ".slider-pager-page { color: " + '#ef473a !important' + "; }";
+   
+    if($rootScope.appTheme && $rootScope.combinationTheme){
+       
+        $scope.colorchnage =$rootScope.appTheme;
+        $scope.colorchnage1 = $rootScope.combinationTheme;
+        //$scope.mystyles2 = ".slider-pager-page.active { color: " + '#11c1f3'+ "; }"+".slider-pager-page { color: " + '#ef473a' + "; }";
+        $scope.mystyles1 = ".slider-pager-page.active { color: " +  $rootScope.combinationTheme + "!important; }";
+        $scope.mystyles2 = ".slider-pager-page { color: " +  $rootScope.appTheme + "!important; }";
+    }
+    else{
+        $scope.mystyles1 = ".slider-pager-page.active { color: " + '#ef473a !important' + "!important; }";
+        $scope.mystyles2 = ".slider-pager-page { color: " + '#11c1f3!important' + "; }";
+    }
+   
     $scope.title=$stateParams.fromWhere;
   $scope.goBack = function() {
     $ionicHistory.goBack();
